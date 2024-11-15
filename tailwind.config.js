97c582821711617930ed860cf0736c6fc4f4aafd
RianFonsecaa <rian.sfonsecaa@gmail.com>
2024-11-15 17:33:33 -0300

    feat(Home Page Form): Validated user register form with CEP request

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#5A2DFF',
+        secondaryColor: '#8E6FFF',
+      }
+    },
+  },
+  plugins: [],
+}
+
